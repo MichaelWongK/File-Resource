7386ec87ac435a0509c76e543145981b608b5858
micheal.wang <wangmk13@163.com>
2020-09-25 10:25:17 +0800

    feat: host change

---
+++ file-mongo/src/test/java/com/micheal/file/mongo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.micheal.file.mongo;
+
+/**
+ * @author <a href="mailto:wangmk13@163.com">micheal.wang</a>
+ * @date 2020/8/4 22:33
+ * @Description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+    }
+}
